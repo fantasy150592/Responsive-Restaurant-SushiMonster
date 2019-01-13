73a45bf0d06591e224d99e2e587a5f9f57ea8344
fantasy150592 <pdphuongit@gmail.com>
2019-01-13 23:19:06 +0700

    change

--- Restaurant-SushiMonster/script.js
+++ Restaurant-SushiMonster/script.js
@@ -1,4 +1,45 @@
 $(document).ready(function() {
+    //toggle btn
+    $('.navbar-toggler').click(function() {
+        $('.navbar-toggler').toggleClass('change');
+    });
+    //sticky navbar less padding
+    $(window).scroll(function() {
+        let position = $(this).scrollTop();
+        console.log(position);
+        if (position >= 718) {
+            $('.navbar').addClass('navbar-background');
+            $('.navbar').addClass('fixed-top');
+        } else {
+            $('.navbar').removeClass('navbar-background');
+            $('.navbar').removeClass('fixed-top');
+        }
+    });
+
+    //smooth scroll
+    $('.nav-item a, .header-link, #back-to-top').click(function(link) {
+
+        link.preventDefault();
+
+        let target = $(this).attr('href');
+
+        $('html, body').stop().animate({
+            scrollTop: $(target).offset().top - 25
+        }, 3000);
+    });
+
+    //back to top
+    $(window).scroll(function() {
+        let position = $(this).scrollTop();
+        console.log(position);
+        if (position >= 718) {
+            $('#back-to-top').addClass('scrollTop');
+
+        } else {
+            $('#back-to-top').removeClass('scrollTop');
+
+        }
+    });
 
     //ripple
     $('#header, .info').ripples({
@@ -16,4 +57,5 @@ $(document).ready(function() {
             enabled: true
         }
     });
+
 });
